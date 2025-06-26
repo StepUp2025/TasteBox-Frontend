b527d9cd04c131ab81a62f4ea8697c1d0ab6bbcd
hyoseong <hyoseong123@naver.com>
2025-06-26 18:05:22 +0900

    feat: 영화 타입 정의 추가

---
+++ src/entities/contents/Moives/types.ts
@@ -0,0 +1,28 @@
+export interface Movie {
+    id: number;
+    poster_path: string | null;
+    title: string;
+    contentType?: 'movie' | 'tvSeries';
+
+}
+
+export interface MoviesResponse {
+    movies: Movie[];
+    page?: number;
+    totalPages?: number;
+}
+
+export interface MovieDetail extends Movie {
+    backdrop_path: string | null;
+    overview: string;
+    adult: boolean;
+    original_language: string;
+    genres: { id: number; name: string }[];
+    status: string;
+    runtime: number;
+    popularity: number;
+    release_date: string;
+    vote_average: number;
+    vote_count: number;
+    production_companies: { id: number; name: string }[];
+  }
