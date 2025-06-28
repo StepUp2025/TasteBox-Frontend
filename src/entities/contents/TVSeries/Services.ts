bd5bbe9d6f5db633efcd6b0263c8f65dd9f8518e
hyoseong <hyoseong123@naver.com>
2025-06-28 19:53:40 +0900

    fix: api 경로 수정

--- src/entities/contents/TVSeries/Services.ts
+++ src/entities/contents/TVSeries/Services.ts
@@ -1,34 +1,34 @@
-import axios from "axios";
-import {TVSeriesResponse, TVSeriesDetail} from "./types"
+import { TVSeriesResponse, TVSeriesDetail } from "./types";
+import { httpClient } from "src/shared/api/http";
 
-const API_URL = 'localhost:PORT/api/tvs'
+const API_URL = '/tvs';
 
-export const fetchTVSeriesByGenre = async (genre_id:string, page = 1) => {
-    const {data} = await axios.get<TVSeriesResponse>(`${API_URL}? genre_id=${genre_id}&page=${page}`);
-    return data;
+export const fetchTVSeriesByGenre = async (genre_id: string, page = 1) => {
+    const response = await httpClient.get(`${API_URL}? genre_id=${genre_id}&page=${page}`);
+    return response.data;
 }
 
 export const fetchPopularTVSeries = async (page = 1) => {
-    const {data} = await axios.get<TVSeriesResponse>(`${API_URL}/popular? page=${page}`);
-    return data;
+    const { response } = await httpClient.get<TVSeriesResponse>(`${API_URL}/popular? page=${page}`);
+    return response.data;
 }
 
 export const fetchOnTheAirTVSeries = async (page = 1) => {
-    const {data} = await axios.get<TVSeriesResponse>(`${API_URL}/on-the-air?page=${page}`);
-    return data;
+    const { response } = await httpClient.get<TVSeriesResponse>(`${API_URL}/on-the-air?page=${page}`);
+    return response.data;
 }
 
 export const fetchTopRatedTVSeries = async (page = 1) => {
-    const {data} = await axios.get<TVSeriesResponse>(`${API_URL}/top-rated?page=${page}`);
-    return data;
+    const { response } = await httpClient.get<TVSeriesResponse>(`${API_URL}/top-rated?page=${page}`);
+    return response.data;
 }
 
-export const fetchTVSeriesDetail = async (id:number) => {
-    const {data} = await axios.get<TVSeriesDetail>(`${API_URL}/${id}`);
-    return data;
+export const fetchTVSeriesDetail = async (id: number) => {
+    const { response } = await httpClient.get<TVSeriesDetail>(`${API_URL}/${id}`);
+    return response.data;
 }
 
 export const fetchRecommendsTVSeries = async (id: string | number) => {
-    const {data} = await axios.get<TVSeriesResponse>(`${API_URL}/${id}/recommends`);
-    return data;
+    const { response } = await httpClient.get<TVSeriesResponse>(`${API_URL}/${id}/recommends`);
+    return response.data;
 }
