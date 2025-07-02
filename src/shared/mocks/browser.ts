4268276bbfae5b3cbf4d75693cc4205d13f6006e
Eunmi Lee <eunoia0235@gmail.com>
2025-07-02 15:46:06 +0900

    Feat: 장르, 콜렉션 테스트 핸들러 모킹 워커에 등록

--- src/shared/mocks/browser.ts
+++ src/shared/mocks/browser.ts
@@ -1,7 +1,14 @@
-import { setupWorker } from "msw/browser";
-import { authHandlers } from "./handlers/authHandlers";
-import { userHandlers } from "./handlers/userHandlers";
+import { setupWorker } from 'msw/browser';
+import { authHandlers } from './handlers/authHandlers';
+import { collectionHandlers } from './handlers/collectionHandlers';
+import { genreHandlers } from './handlers/genreHandlers';
+import { userHandlers } from './handlers/userHandlers';
 
-const handlers = [...authHandlers, ...userHandlers];
+const handlers = [
+  ...authHandlers,
+  ...userHandlers,
+  ...collectionHandlers,
+  ...genreHandlers,
+];
 
 export const worker = setupWorker(...handlers);
