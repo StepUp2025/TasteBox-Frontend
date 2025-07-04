01bde02d9b1979dd1da201bce5203cd238f09568
hyoseong <hyoseong123@naver.com>
2025-07-04 18:06:25 +0900

    Fix: StyledTitle 수정

--- src/shared/ui/Title/Title.tsx
+++ src/shared/ui/Title/Title.tsx
@@ -1,20 +1,16 @@
 import { HTMLAttributes, JSX, ReactNode } from 'react';
-import { ColorKey, FontSizeKey } from 'shared/types/theme';
+import { FontSizeKey } from 'shared/types/theme';
 import styled from 'styled-components';
 
 interface TitleProps extends HTMLAttributes<HTMLElement> {
   children: ReactNode;
   size: FontSizeKey;
-  color?: ColorKey;
   as?: keyof JSX.IntrinsicElements;
 }
 
-const StyledTitle = styled.span<Pick<TitleProps, 'size' | 'color'>>`
-  font-family: 'Work Sans', sans-serif;
+const StyledTitle = styled.span<Omit<TitleProps, 'children'>>`
   font-weight: bold;
   font-size: ${({ theme, size = 'large' }) => theme.fontSize[size]};
-  color: ${({ theme, color }) => (color ? theme.color[color] : 'black')};
-  letter-spacing: -0.02em;
 `;
 
 export default function Title({
