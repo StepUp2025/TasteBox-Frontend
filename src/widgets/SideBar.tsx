5519f6d21c7c9eb0929273abdd7e8c7b4b54456c
hyoseong <hyoseong123@naver.com>
2025-07-07 06:12:31 +0900

    Feat: SideBar 구현현

---
+++ src/widgets/SideBar.tsx
@@ -0,0 +1,158 @@
+import { useThemeStore } from 'entities/theme/model/store/themeStore';
+import {
+  Clapperboard,
+  Folder,
+  LogIn,
+  PackageOpen,
+  Sun,
+  Tv,
+  User,
+} from 'lucide-react';
+import { Button } from 'shared/ui';
+import styled from 'styled-components';
+
+export default function Sidebar() {
+  const { toggleTheme } = useThemeStore();
+
+  return (
+    <SidebarWrapper>
+      <Top>
+        <Button
+          buttonSize="menuNarrow"
+          fontSize="large"
+          scheme="primary"
+          borderRadius="medium"
+        >
+          <PackageOpen size={32} />
+        </Button>
+        <Nav>
+          <Button
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <MenuContent>
+              <Clapperboard size={24} />
+              <Label>영화</Label>
+            </MenuContent>
+          </Button>
+          <Button
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <MenuContent>
+              <Tv size={24} />
+              <Label>TV 시리즈</Label>
+            </MenuContent>
+          </Button>
+
+          <Button
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <MenuContent>
+              <Folder size={24} />
+              <Label>컬렉션</Label>
+            </MenuContent>
+          </Button>
+
+          <Button
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <MenuContent>
+              <User size={24} />
+              <Label>마이페이지</Label>
+            </MenuContent>
+          </Button>
+        </Nav>
+      </Top>
+
+      <Bottom>
+        <Button
+          buttonSize="menuNarrow"
+          fontSize="small"
+          scheme="menu"
+          borderRadius="medium"
+        >
+          <LogIn size={22} />
+        </Button>
+
+        <Button
+          onClick={toggleTheme}
+          buttonSize="menuNarrow"
+          fontSize="small"
+          scheme="menu"
+          borderRadius="medium"
+        >
+          <Sun size={22} />
+        </Button>
+      </Bottom>
+    </SidebarWrapper>
+  );
+}
+
+const SidebarWrapper = styled.aside`
+  width: auto;
+  height: 100vh;
+  background: ${({ theme }) => theme.color.basicBackground};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border-right: 1px solid ${({ theme }) => theme.color.border};
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 10;
+`;
+
+const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 8px;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  align-items: center;
+  margin-top: 32px;
+`;
+
+const MenuContent = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 8px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Label = styled.span`
+  margin-top: 4px;
+  font-size: ${({ theme }) => theme.fontSize.xsmall};
+  color: ${({ theme }) => theme.color.secondText};
+  text-align: center;
+  width: 100%;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+  align-items: center;
+  margin-bottom: 24px;
+  padding: 15px;
+`;
