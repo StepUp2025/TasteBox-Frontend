f0b76e173bac867859e52ea49746de12c5df1c9d
Eunmi Lee <eunoia0235@gmail.com>
2025-07-07 13:42:44 +0900

    Feat: Error 페이지 내보내기 추가

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './Button/Button';
+export * from './Error/ErrorBox';
 export * from './InputText/InputText';
 export * from './Modal/Modal';
 export * from './ProgressBar/ProgressBar';
