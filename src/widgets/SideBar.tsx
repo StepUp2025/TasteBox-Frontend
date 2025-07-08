592d03865255b1195eacf472a0bb973a0256f86c
hyoseong <hyoseong123@naver.com>
2025-07-08 23:58:19 +0900

    Refactor: Active 및 로그인 부분 구현

--- src/widgets/SideBar.tsx
+++ src/widgets/SideBar.tsx
@@ -1,20 +1,35 @@
+import { useAuthStore } from 'entities/auth/model/store/authStore';
 import { useThemeStore } from 'entities/theme/model/store/themeStore';
 import {
   Clapperboard,
   Folder,
   LogIn,
+  LogOut,
   PackageOpen,
   Sun,
   Tv,
   User,
 } from 'lucide-react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Button, IconPreset } from 'shared/ui';
 import styled from 'styled-components';
 
+interface LabelProps {
+  $active?: boolean;
+}
+
 export default function Sidebar() {
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn());
+  const resetAccessToken = useAuthStore((state) => state.resetAccessToken);
   const { toggleTheme } = useThemeStore();
   const navigate = useNavigate();
+  const handleLogout = () => {
+    resetAccessToken();
+    localStorage.removeItem('accessToken');
+    navigate('/');
+  };
+  const location = useLocation();
+  const isActive = (path) => location.pathname === path;
 
   return (
     <SidebarWrapper>
@@ -26,7 +41,7 @@ export default function Sidebar() {
           scheme="primary"
           borderRadius="medium"
         >
-          <IconPreset width={32} color={'constantWhite'}>
+          <IconPreset width={24} color={'constantWhite'}>
             <PackageOpen width={24} />
           </IconPreset>
         </Button>
@@ -37,10 +52,14 @@ export default function Sidebar() {
             fontSize="small"
             scheme="menu"
             borderRadius="medium"
+            $active={isActive('/movie')}
           >
             <MenuContent>
-              <Clapperboard size={24} />
-              <Label>영화</Label>
+              <Clapperboard
+                size={24}
+                stroke={isActive('/movie') ? '#111' : '#888'}
+              />
+              <Label $active={isActive('/movie')}>영화</Label>
             </MenuContent>
           </Button>
           <Button
@@ -49,49 +68,75 @@ export default function Sidebar() {
             fontSize="small"
             scheme="menu"
             borderRadius="medium"
+            $active={isActive('/tv')}
           >
             <MenuContent>
-              <Tv size={24} />
-              <Label>TV 시리즈</Label>
+              <Tv size={24} stroke={isActive('/tv') ? '#111' : '#888'} />
+              <Label $active={isActive('/tv')}>TV 시리즈</Label>
             </MenuContent>
           </Button>
-          <Button
-            onClick={() => navigate('/collection')}
-            buttonSize="menuNarrow"
-            fontSize="small"
-            scheme="menu"
-            borderRadius="medium"
-          >
-            <MenuContent>
-              <Folder size={24} />
-              <Label>컬렉션</Label>
-            </MenuContent>
-          </Button>
-          <Button
-            onClick={() => navigate('/mypage')}
-            buttonSize="menuNarrow"
-            fontSize="small"
-            scheme="menu"
-            borderRadius="medium"
-          >
-            <MenuContent>
-              <User size={24} />
-              <Label>마이페이지</Label>
-            </MenuContent>
-          </Button>
+          {isLoggedIn && (
+            <>
+              <Button
+                onClick={() => navigate('/collection')}
+                buttonSize="menuNarrow"
+                fontSize="small"
+                scheme="menu"
+                borderRadius="medium"
+                $active={isActive('/collection')}
+              >
+                <MenuContent>
+                  <Folder
+                    size={24}
+                    stroke={isActive('/collection') ? '#111' : '#888'}
+                  />
+                  <Label $active={isActive('/collection')}>컬렉션</Label>
+                </MenuContent>
+              </Button>
+
+              <Button
+                onClick={() => navigate('/mypage')}
+                buttonSize="menuNarrow"
+                fontSize="small"
+                scheme="menu"
+                borderRadius="medium"
+                $active={isActive('/mypage')}
+              >
+                <MenuContent>
+                  <User
+                    size={24}
+                    stroke={isActive('/mypage') ? '#111' : '#888'}
+                  />
+                  <Label $active={isActive('/mypage')}>마이페이지</Label>
+                </MenuContent>
+              </Button>
+            </>
+          )}
         </Nav>
       </Top>
 
       <Bottom>
-        <Button
-          onClick={() => navigate('/login')}
-          buttonSize="menuNarrow"
-          fontSize="small"
-          scheme="menu"
-          borderRadius="medium"
-        >
-          <LogIn size={24} />
-        </Button>
+        {isLoggedIn ? (
+          <Button
+            onClick={handleLogout}
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <LogOut size={24} />
+          </Button>
+        ) : (
+          <Button
+            onClick={() => navigate('/login')}
+            buttonSize="menuNarrow"
+            fontSize="small"
+            scheme="menu"
+            borderRadius="medium"
+          >
+            <LogIn size={24} stroke={'#888'} />
+          </Button>
+        )}
 
         <Button
           onClick={toggleTheme}
@@ -100,7 +145,7 @@ export default function Sidebar() {
           scheme="menu"
           borderRadius="medium"
         >
-          <Sun size={24} />
+          <Sun size={24} stroke={'#888'} />
         </Button>
       </Bottom>
     </SidebarWrapper>
@@ -148,10 +193,11 @@ const MenuContent = styled.span`
   height: 100%;
 `;
 
-const Label = styled.span`
+const Label = styled.span<LabelProps>`
   margin-top: 4px;
   font-size: ${({ theme }) => theme.fontSize.xsmall};
-  color: ${({ theme }) => theme.color.secondText};
+  color: ${({ $active }) => ($active ? '#111' : '#888')};
+  font-weight: ${({ $active }) => ($active ? 'bold' : 'normal')};
   text-align: center;
   width: 100%;
 `;
