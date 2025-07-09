0920f2f311e2a1dc6e9a27bbf12dacc8d8927dab
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-07-09 21:34:46 +0900

    Feat: 유저 프로필 위젯 작성

--- src/widgets/user/userProfile/UserProfileSettingWidget.tsx
+++ src/widgets/user/userProfile/UserProfileSettingWidget.tsx
@@ -1,7 +1,9 @@
 import UpdatePasswordModal from 'features/auth/updatePassword/ui/UpdatePasswordModal';
+import UserProfileForm from 'features/user/profile/ui/UserProfileForm';
 import { useState } from 'react';
 import { Button } from 'shared/ui';
 import styled from 'styled-components';
+import { MYPAGE_HEADER_MARGIN } from '../constants';
 
 const UserProfileSettingWidget = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -9,21 +11,42 @@ const UserProfileSettingWidget = () => {
   const handleCloseModal = () => setIsModalOpen(false);
   return (
     <UserProfileSettingStyle>
-      <h1>UserProfileSetting</h1>
-      <Button
-        buttonSize="large"
-        fontSize="xsmall"
-        scheme="secondary"
-        borderRadius="medium"
-        onClick={handleOpenModal}
-      >
-        비밀번호 수정
-      </Button>
+      <h2>프로필</h2>
+      <div className="form-container">
+        <UserProfileForm />
+        <Button
+          buttonSize="large"
+          fontSize="xsmall"
+          scheme="secondary"
+          borderRadius="medium"
+          onClick={handleOpenModal}
+        >
+          비밀번호 수정
+        </Button>
+      </div>
       <UpdatePasswordModal open={isModalOpen} onClose={handleCloseModal} />
     </UserProfileSettingStyle>
   );
 };
 
-const UserProfileSettingStyle = styled.div``;
+const UserProfileSettingStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+    h2 {
+    align-self: flex-start;
+    margin-left: ${MYPAGE_HEADER_MARGIN};
+    margin-bottom: 24px;
+  }
+
+  .form-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    min-width: 520px;
+    margin-top: 24px;
+  }
+`;
 
 export default UserProfileSettingWidget;
