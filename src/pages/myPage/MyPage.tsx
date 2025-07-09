950221b348887ebbdeda1c04a3d1baac43a438ab
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-07-10 08:54:31 +0900

    Feat: 마이페이지 구현

--- src/pages/myPage/MyPage.tsx
+++ src/pages/myPage/MyPage.tsx
@@ -1,3 +1,13 @@
+import UserCollectionList from 'widgets/user/userProfile/UserCollectionList';
+import UserPreferenceWidget from 'widgets/user/userProfile/UserPreferenceWidget';
+import UserProfileSettingWidget from 'widgets/user/userProfile/UserProfileSettingWidget';
+
 export default function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div>
+      <UserProfileSettingWidget />
+      <UserPreferenceWidget />
+      <UserCollectionList />
+    </div>
+  );
 }
