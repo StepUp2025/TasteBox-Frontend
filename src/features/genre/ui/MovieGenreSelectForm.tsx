328b25501ceb0053048f70f8b6f6a82689e2e370
Eunmi Lee <eunoia0235@gmail.com>
2025-07-11 05:11:28 +0900

    Feat: 장르 선택 제출 컴포넌트 구현
    - location에서 받는 상태값 from을 통해 navigate (로그인 -> 메인, 마이페이지 -> 마이페이지)
    - 장르 최소 1개 이상 선택
    - 선택한 장르 session으로 저장하여 뒤로가기나 이전 버튼 클릭 시에 데이터 유실하지 않도록 함
    - movie 세션 값 없이 tv 진입 시( movie 페이지 거치지 않은 비정상 루트)에 movie 페이지로 강제 이동

---
+++ src/features/genre/ui/MovieGenreSelectForm.tsx
@@ -0,0 +1,75 @@
+// MovieGenreForm.tsx
+
+import { useUserPreference } from 'features/user/preference/hooks/useGetUserPreference';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button } from 'shared/ui';
+import { toast } from 'sonner';
+import { ButtonWrapper, GenreSelectForm } from '../style/GenreSelectPage.style';
+import { GenreSelector } from './GenreSelector';
+
+export const MovieGenreSelectForm = () => {
+  const [selectedGenres, setSelectedGenres] = useState<number[]>(() => {
+    const saved = sessionStorage.getItem('selectedMovieGenres');
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from;
+
+  const { data: preferenceData } = useUserPreference();
+
+  useEffect(() => {
+    const saved = sessionStorage.getItem('selectedMovieGenres');
+    const hasSaved = saved && JSON.parse(saved).length > 0;
+    if (!preferenceData || hasSaved) return;
+
+    const genres = preferenceData.movies.genres.map((g) => g.id);
+    setSelectedGenres(genres);
+    sessionStorage.setItem('selectedMovieGenres', JSON.stringify(genres));
+  }, [preferenceData]);
+
+  useEffect(() => {
+    sessionStorage.setItem(
+      'selectedMovieGenres',
+      JSON.stringify(selectedGenres),
+    );
+  }, [selectedGenres]);
+
+  const handleNext = () => {
+    if (selectedGenres.length === 0) {
+      toast.error('영화 장르를 최소 1개 이상 선택해주세요.');
+      return;
+    }
+
+    navigate('/genre/tv', {
+      state: {
+        from,
+      },
+    });
+  };
+
+  return (
+    <GenreSelectForm>
+      <GenreSelector
+        type="movie"
+        selectedIds={selectedGenres}
+        onSelect={setSelectedGenres}
+      />
+
+      <ButtonWrapper>
+        <Button
+          onClick={handleNext}
+          fontSize="small"
+          disabled={selectedGenres.length === 0}
+          scheme="primary"
+          buttonSize="medium"
+          borderRadius="round"
+        >
+          다음
+        </Button>
+      </ButtonWrapper>
+    </GenreSelectForm>
+  );
+};
