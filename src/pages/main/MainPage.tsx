be1f7b6a1773acff7cf1dd55c5d12026ac61a761
hyoseong <hyoseong123@naver.com>
2025-07-11 18:01:01 +0900

    Refactor: 메인페이지 수정

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,68 +1,73 @@
 import { useAuthStore } from 'entities/auth/model/store/authStore';
-import { useLatestContents } from 'features/contents/hooks/latest/latest';
+import { useLatestContents } from 'features/contents/hooks/latest/useLatestContents';
 import { useMovieRecommends } from 'features/contents/hooks/movie/useGetMovieRecommends';
 import { usePopularMovies } from 'features/contents/hooks/movie/useGetPopularMovies';
 import { usePopularTVs } from 'features/contents/hooks/tvs/useGetPopularTVs';
 import { useRecommendsTVs } from 'features/contents/hooks/tvs/useGetRecommendsTVs';
-import ContentsList from 'features/contents/ui/ContentsList/ContentsList';
+import ContentsListViewer from 'features/contents/ui/ContentsList/ContentListViewer';
+import styled from 'styled-components';
 
 export default function MainPage() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn());
-  const { data, isPending } = useLatestContents(10);
+  const { data } = useLatestContents(10);
   const latestContents = data?.contents || [];
-  const { data: popularMoviesData, isPending: popularRecPending } =
-    usePopularMovies();
+  const { data: popularMoviesData } = usePopularMovies();
   const popularMovies = popularMoviesData?.contents || [];
-  const { data: popularTVsData, isPending: popularTVsPending } =
-    usePopularTVs();
+  const { data: popularTVsData } = usePopularTVs();
   const popularTVs = popularTVsData?.contents || [];
 
   const recommendMovieId = 1;
   const recommendTVId = 5;
 
-  const { data: movieRecommends, isPending: movieRecPending } =
-    useMovieRecommends(recommendMovieId);
-  const { data: tvRecommends, isPending: tvRecPending } =
-    useRecommendsTVs(recommendTVId);
+  const { data: movieRecommendsData } = useMovieRecommends(recommendMovieId);
+  const movieRecommends = movieRecommendsData?.contents || [];
+
+  const { data: tvRecommendsData } = useRecommendsTVs(recommendTVId);
+  const tvRecommends = tvRecommendsData?.contents || [];
 
   return (
     <>
       {isLoggedIn ? (
-        <>
-          <ContentsList
+        <MaintWrapper>
+          <ContentsListViewer
             title="최근 추가한 컨텐츠"
-            contents={isPending ? [] : latestContents}
+            contents={latestContents}
             type="scroll"
           />
-          <ContentsList
+          <ContentsListViewer
             title="추천 영화"
-            contents={movieRecPending ? [] : movieRecommends?.contents || []}
+            contents={movieRecommends}
             type="link"
             linkTo="movie"
           />
-          <ContentsList
+          <ContentsListViewer
             title="추천 TV시리즈"
-            contents={tvRecPending ? [] : tvRecommends?.contents || []}
+            contents={tvRecommends}
             type="link"
             linkTo="tv"
           />
-        </>
+        </MaintWrapper>
       ) : (
-        <>
-          <ContentsList
+        <MaintWrapper>
+          <ContentsListViewer
             title="인기 영화"
-            contents={popularRecPending ? [] : popularMovies}
+            contents={popularMovies}
             type="link"
             linkTo="movie"
           />
-          <ContentsList
+          <ContentsListViewer
             title="인기 TV시리즈"
-            contents={popularTVsPending ? [] : popularTVs}
+            contents={popularTVs}
             type="link"
             linkTo="tv"
           />
-        </>
+        </MaintWrapper>
       )}
     </>
   );
 }
+
+const MaintWrapper = styled.div`
+margin-left: 7rem;
+  padding-left: 7rem;
+`;
