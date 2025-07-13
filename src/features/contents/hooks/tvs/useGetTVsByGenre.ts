2cdcf3d0156cd995a7e2e5e733fccf65976380e1
hyoseong <hyoseong123@naver.com>
2025-07-14 03:35:48 +0900

    Refactor: 파라미터 값 수정

--- src/features/contents/hooks/tvs/useGetTVsByGenre.ts
+++ src/features/contents/hooks/tvs/useGetTVsByGenre.ts
@@ -2,9 +2,14 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchTVsByGenre } from 'entities/contents/model';
 import { ContentsResponse } from 'entities/contents/model/types/contents.type';
 
-export function useTVsByGenre(genreIds: number[], page = 1, limit = 18) {
+export function useTVsByGenre(
+  genreIds: number[] = [],
+  page: number = 1,
+  limit: number = 18,
+) {
   return useQuery<ContentsResponse>({
     queryKey: ['tvs', genreIds, page, limit],
     queryFn: () => fetchTVsByGenre(genreIds, page, limit),
+    enabled: genreIds.length > 0,
   });
 }
