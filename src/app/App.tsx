916651af05c3fecd13efb9854800039da08df15e
Eunmi Lee <eunoia0235@gmail.com>
2025-07-15 17:24:03 +0900

    Feat: 페이지 바뀔 때마다 맨 위에서 렌딩되도록 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProviders } from 'app/themeProvider';
 import { BrowserRouter } from 'react-router-dom';
 import { queryClient } from 'shared/lib/queryClient';
+import ScrollToTop from 'shared/ui/ScrollToTop/ScrollToTop';
 import { AuthProvider } from './AuthProvider';
 import { Routers } from './Routers';
 
@@ -11,6 +12,7 @@ function App() {
       <AuthProvider>
         <ThemeProviders>
           <BrowserRouter>
+            <ScrollToTop />
             <Routers />
           </BrowserRouter>
         </ThemeProviders>
