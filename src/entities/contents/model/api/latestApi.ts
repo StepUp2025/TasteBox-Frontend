c174ce1081d627da4ff3ba4965203d9a436d198e
hyoseong <hyoseong123@naver.com>
2025-07-16 17:00:03 +0900

    Refactor: Latest 타입 수정

--- src/entities/contents/model/api/latestApi.ts
+++ src/entities/contents/model/api/latestApi.ts
@@ -1,9 +1,9 @@
-import { ContentsResponse } from 'entities/contents/model/types/contents.type';
 import { authClient } from 'shared/api';
+import { LatestResponse } from '../types/contents.type';
 
 // 최근 추가한 컨텐츠 조회
 export const fetchLatestContents = async (limit: number = 10) => {
-  const response = await authClient.get<ContentsResponse>('/contents/latest', {
+  const response = await authClient.get<LatestResponse>('/contents/latest', {
     params: { limit },
   });
   return response.data;
