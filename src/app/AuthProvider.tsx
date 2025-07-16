fc0ed4e4469cd317430cb9a567d67cd492b0e7f9
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2025-07-17 02:56:49 +0900

    Fix: AuthProvicer 안쓰는 변수 삭제

--- src/app/AuthProvider.tsx
+++ src/app/AuthProvider.tsx
@@ -1,11 +1,9 @@
-import { useAuthStore } from 'entities/auth/model/store/authStore';
 import { useRefreshToken } from 'features/auth/refreshToken/hooks/useRefreshToken';
 import { useEffect } from 'react';
 import Loading from 'shared/ui/Loading/Loading';
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const { refresh, isPending } = useRefreshToken();
-  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
 
   useEffect(() => {
     refresh();
