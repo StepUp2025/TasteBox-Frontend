778ffcbc4ed72d8ca804bca67b1258ac3414853b
hyoseong <hyoseong123@naver.com>
2025-07-17 17:16:12 +0900

    Refactor: Tv디테일 수정

--- src/pages/tv/TVDetailPage.tsx
+++ src/pages/tv/TVDetailPage.tsx
@@ -1,100 +1,136 @@
-import { ContentType } from 'entities/contents/model/types/contents.type';
+import { useAddCollectionContents } from 'features/collection/hooks/useAddCollectionContents';
 import { useRecommendsTVs } from 'features/contents/hooks/tvs/useGetRecommendsTVs';
 import { useTVDetail } from 'features/contents/hooks/tvs/useGetTVDetail';
-import ContentsList from 'features/contents/ui/ContentsList/ContentsList';
+import ContentsListViewer from 'features/contents/ui/ContentsList/ContentListViewer';
+import ModalItem from 'features/contents/ui/modal/modal';
+import SeasonListViewer from 'features/contents/ui/SeasonList/seasonListViewe';
 import { Calendar, Earth, Plus, Star } from 'lucide-react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Button, Title } from 'shared/ui';
-import styled, { useTheme } from 'styled-components';
+import defaultContentsImage from 'shared/assets/images/default-contents-image.png';
+import { BackgroundImage } from 'shared/styles/backgroundStyle';
+import { Button, Modal, Title } from 'shared/ui';
+import Loading from 'shared/ui/Loading/Loading';
+import styled from 'styled-components';
 
 export default function TVDetailPage() {
-  const theme = useTheme();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
   const { id } = useParams();
   const tvId = Number(id);
-  const { data, isPending, isError, error } = useTVDetail(tvId);
-  const { data: tvRecommends, isPending: tvRecPending } =
-    useRecommendsTVs(tvId);
-  if (isPending) return <div>로딩 중...</div>;
-  if (isError) return <div>에러 발생: {error?.message}</div>;
-  if (!data) return <div>TV 정보를 찾을 수 없습니다.</div>;
 
-  const seasonContents = data.seasons?.map((season) => ({
-    id: season.id,
-    title: season.title,
-    posterPath: season.posterPath,
-    contentType: 'tv' as ContentType,
-  }));
+  const { data, isPending, isError, error } = useTVDetail(tvId);
+  const { data: tvRecommendsData } = useRecommendsTVs(1, 18);
+  const tvRecommends = tvRecommendsData?.contents || [];
+
+  const { mutate: addToCollections } = useAddCollectionContents(tvId);
+
+  const handleOpenModal = () => {
+    const isLoggedIn = !!localStorage.getItem('accessToken');
+
+    if (!isLoggedIn) {
+      alert('로그인이 필요한 기능입니다. 로그인 후 이용해주세요.');
+      return;
+    }
+
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setSelectedIds([]); // 모달 닫을 때 선택 초기화
+  };
+  const handleModalSave = () => {
+    if (selectedIds.length === 0) {
+      alert('저장할 컬렉션을 선택해주세요.');
+      return;
+    }
+    addToCollections(selectedIds, {
+      onSuccess: handleCloseModal,
+      onError: () => alert('저장에 실패했습니다.'),
+    });
+  };
+
+  if (isPending) return <Loading />;
+  if (isError) return <div>에러 발생: {error?.message}</div>;
+  if (!data) return <div>TV 시리즈 정보를 찾을 수 없습니다.</div>;
 
   const {
     title,
     genres,
-    status,
-    firstAirDate,
-    lastAirDate,
     overview,
     voteAverage,
     voteCount,
     originalLanguage,
     posterPath,
     backdropPath,
+    firstAirDate,
+    lastAirDate,
     numberOfSeasons,
+    seasons,
   } = data;
+
   return (
     <Wrapper>
-      <BackgroundPoster
-        src={backdropPath || '/default-tv.png'}
-        alt="background"
-      />
+      <BackgroundImage $imageUrl={backdropPath || undefined} />
       <HeaderSection>
-        <Poster src={posterPath || '/default-tv.png'} alt={title} />
+        <Poster
+          src={posterPath ? posterPath : defaultContentsImage}
+          alt={title}
+        />
         <Info>
-          <Title>{title} </Title>
-          {status}
-
-          <InfoRow>{genres?.map((g) => g.name).join(', ')}</InfoRow>
+          <Title>{title}</Title>
+          <InfoRow>{genres?.map((g) => g.name).join(' · ')}</InfoRow>
           <InfoRow>
             <Star size={24} />
             {voteAverage}({voteCount})
-            <Calendar size={24} /> {firstAirDate}-{lastAirDate}
+            <Calendar size={24} /> {firstAirDate}~{lastAirDate}
           </InfoRow>
           <InfoRow>
-            <Earth size={24} /> {originalLanguage}
+            <Earth size={24} /> {originalLanguage?.toUpperCase()}
           </InfoRow>
-          <Button
-            onClick={() => {}}
+          <CollectionButton
+            onClick={handleOpenModal}
             disabled={isPending}
             buttonSize="small"
             fontSize="small"
-            scheme="menu"
+            scheme="primary"
             borderRadius="large"
-            disableHoverOverlay={true}
           >
-            <CollectionButton>
-              <Plus size={24} stroke={theme.color.primary} />
-              컬렉션 추가
-            </CollectionButton>
-          </Button>
+            <Plus size={24} /> <p>컬렉션 추가</p>
+          </CollectionButton>
+          <Modal
+            open={isModalOpen}
+            onClose={handleCloseModal}
+            title="콘텐츠 저장"
+            confirmText="추가하기"
+            confirmType="button"
+            onConfirm={handleModalSave}
+          >
+            <ModalItem
+              selectedIds={selectedIds}
+              setSelectedIds={setSelectedIds}
+            />
+          </Modal>
         </Info>
       </HeaderSection>
-
       <OverviewSection>
         <Title>줄거리</Title>
         {overview}
       </OverviewSection>
-      <SeasonSection>
-        <Title> 시즌 ({numberOfSeasons}) </Title>
-        <ContentsList
-          title=""
-          contents={seasonContents}
-          type="link"
-          linkTo="tv"
+      <SeasonsSection>
+        <SeasonListViewer
+          title="시즌"
+          seasons={seasons.map((season) => ({
+            ...season,
+          }))}
+          type="scroll"
+          numberOfSeasons={numberOfSeasons}
         />
-      </SeasonSection>
+      </SeasonsSection>
       <RecommendSection>
-        <Title>추천 TV 시리즈</Title>
-        <ContentsList
-          title=""
-          contents={tvRecPending ? [] : tvRecommends?.contents || []}
+        <ContentsListViewer
+          title="추천TV시리즈"
+          contents={tvRecommends}
           type="link"
           linkTo="tv"
         />
@@ -102,87 +138,85 @@ export default function TVDetailPage() {
     </Wrapper>
   );
 }
-const SidebarWidth = 107;
-const Wrapper = styled.div`
- position: relative;
-  padding: 32px;
-  margin: 0;
-  margin-left: 90px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  z-index: 99;
-`;
 
-const BackgroundPoster = styled.img`
-position: fixed;
-margin-left: ${SidebarWidth}px;  
-width: calc(100vw - ${SidebarWidth}px);  
-top: 0;
-left: 0;
-height: 40%;
-object-fit: cover;
-z-index: 1;
-opacity: 0.3;
-pointer-events: none;
+const Wrapper = styled.div`
+position: relative;
+padding: 32px;
+margin: 0;
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+z-index: 99;
 `;
 
 const HeaderSection = styled.section`
-  display: flex;
-  gap: 32px;
-  align-items: flex-start;
+position: relative;
+display: flex;
+gap: 32px;
+align-items: flex-end;
+z-index: 3;
 `;
 
 const Poster = styled.img`
-  width: 220px;
-  height: 320px;
-  border-radius: ${({ theme }) => theme.borderRadius.medium};
-  box-shadow: ${({ theme }) => theme.shadow.default};
-  object-fit: cover;
+width: 220px;
+height: 320px;
+border-radius: ${({ theme }) => theme.borderRadius.medium};
+object-fit: cover;
+margin-top: 60px;
 `;
 
 const Info = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
+display: flex;
+flex-direction: column;
+gap: 8px;
 `;
 
 const InfoRow = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  margin-top: 20px;
-  font-size: ${({ theme }) => theme.fontSize.small};
+display: flex;
+align-items: center;
+gap: 8px;
+margin-top: 20px;
+font-size: ${({ theme }) => theme.fontSize.small};
 `;
 
 const OverviewSection = styled.section`
-  margin: 40px 0 24px 0;
-  display: flex;
-  flex-direction:column;
-  gap: 20px;
-`;
-
-const SeasonSection = styled.section`
-  margin: 40px 0 24px 0;
+margin: 40px 0 24px 0;
+display: flex;
+flex-direction:column;
+gap: 20px;
 `;
 
 const RecommendSection = styled.section`
-  margin: 40px 0 24px 0;
+margin: 40px 0 24px 0;
 `;
 
-const CollectionButton = styled.button`
-  color: ${({ theme }) => theme.color.primary};
-  border: 1.5px solid ${({ theme }) => theme.color.primary};
-  border-radius: ${({ theme }) => theme.borderRadius.large};
-  font-size: ${({ theme }) => theme.fontSize.medium};
-  padding: 6px 18px;
-  display: flex;
-  align-items: center;
-  margin-top:20px;
-  gap: 6px;
-  transition: background 0.2s;
-  &:hover {
-    background: ${({ theme }) => theme.color.subBackground};
-    font-weight: bold;
-}
+const SeasonsSection = styled.section`
+display: flex;
+align-items: center;
+flex-direction: column;
+margin-top: 4rem;
+width: 100%;
+`;
+
+const CollectionButton = styled(Button)`
+width: 200px;
+height: 43px;
+margin-top: 40px;
+gap: 6px;
+display: flex;
+justify-content: center;
+align-items: center;
+white-space: nowrap;
+
+`;
+
+export const ModalContainer = styled.div`
+background: ${({ theme }) => theme.color.constantWhite};
+padding: 24px;
+border-radius: ${({ theme }) => theme.borderRadius.medium};
+position: relative;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 `;
