2713b403a14a4db140aa18712ba9361e9d776fa1
hyoseong <hyoseong123@naver.com>
2025-07-18 06:12:07 +0900

    Refactor: RemoveHooks 수정

--- src/features/collection/hooks/useRemoveCollectionContents.ts
+++ src/features/collection/hooks/useRemoveCollectionContents.ts
@@ -3,17 +3,25 @@ import { AxiosError } from 'axios';
 import { removeCollectionContents } from 'entities/collection';
 import { CustomErrorResponse } from 'shared/types/CustomErrorResponse';
 
-export const useRemoveCollectionContents = (id: number) => {
+type RemoveCollectionPayload = {
+  collectionId: number;
+  contentIds: number[];
+};
+
+export const useRemoveCollectionContents = () => {
   const queryClient = useQueryClient();
 
   const { mutate, isPending, isSuccess, isError, error } = useMutation<
     string,
     AxiosError<CustomErrorResponse>,
-    number[]
+    RemoveCollectionPayload
   >({
-    mutationFn: (contentIds) => removeCollectionContents(id, contentIds),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['collections', id] });
+    mutationFn: ({ collectionId, contentIds }) =>
+      removeCollectionContents(collectionId, contentIds),
+    onSuccess: (_, { collectionId }) => {
+      queryClient.invalidateQueries({
+        queryKey: ['collections', collectionId],
+      });
     },
   });
 
